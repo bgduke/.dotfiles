233e802f3de41e5f1952c2dc1372d46ad8d9202a
Breno Duque <breno.ga.duque@gmail.com>
2026-06-03 22:31:52 -0300

    fixed quit behaviour, added smarQuit function to <leader>b

--- configs/.config/nvim/lua/core/keymaps.lua
+++ configs/.config/nvim/lua/core/keymaps.lua
@@ -15,7 +15,47 @@ vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear Highlighting
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save Buffer" })
 vim.keymap.set("n", "H", ":bprevious<CR>", { silent = true })
 vim.keymap.set("n", "L", ":bnext<CR>", { silent = true })
-vim.keymap.set("n", "<leader>q", ":w | bdelete<CR>", { silent = true })
+local function smart_quit()
+	local current = vim.api.nvim_get_current_buf()
+
+	if vim.bo[current].buftype ~= "" then
+		vim.cmd("quit")
+		return
+	end
+
+	vim.cmd("write")
+
+	local buffers = vim.tbl_filter(function(bufnr)
+		return bufnr ~= current
+			and vim.api.nvim_buf_is_valid(bufnr)
+			and vim.bo[bufnr].buflisted
+			and vim.bo[bufnr].buftype == ""
+	end, vim.api.nvim_list_bufs())
+
+	if #buffers == 0 then
+		vim.cmd("quit")
+		return
+	end
+
+	local alternate = vim.fn.bufnr("#")
+	local target = buffers[1]
+
+	if alternate > 0 and vim.tbl_contains(buffers, alternate) then
+		target = alternate
+	end
+
+	vim.api.nvim_set_current_buf(target)
+
+	local ok, err = pcall(vim.cmd, "bdelete " .. current)
+	if not ok then
+		vim.api.nvim_set_current_buf(current)
+		vim.notify(err, vim.log.levels.ERROR)
+	end
+end
+
+vim.api.nvim_create_user_command("SmartQuit", smart_quit, {})
+
+vim.keymap.set("n", "<leader>q", ":SmartQuit<CR>", { silent = true })
 vim.keymap.set("n", "<leader>x", ":bdelete<CR>", { desc = "Close Buffer", silent = true })
 vim.keymap.set("n", "<leader>D", vim.diagnostic.open_float, { desc = "Show diagnostics" })
 vim.keymap.set("n", "<C-b>", ":Dotnet build<CR>", { desc = "Build Project" })
